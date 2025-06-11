f8e9370a33e599b04374ce3c4edecb77151862f6
MyDariell <Dari3llsugiaman@gmail.com>
2025-06-11 08:07:12 -0700

    task 1 done

---
+++ rtl/RamInitializer.sv
@@ -0,0 +1,34 @@
+module RamInitializer (clock, address, wren, data, finished, reset);
+
+    input clock, reset;
+    output reg wren = 1'b0; 
+    output reg [7:0] address = 1'b0; 
+    output reg [7:0] data    = 1'b0; 
+    output reg finished      = 1'b0;
+    
+    reg [7:0] next_address = 1'b0; 
+
+    always_ff @( posedge clock ) begin 
+        if (reset) begin
+            wren <= 0; 
+            data <= 0; 
+            address <= 0; 
+            finished <= 0;
+        end
+        else begin
+            if (!finished) begin
+                wren <= 1'b1;
+                address <= next_address;
+                data <= next_address;
+                next_address <= address + 8'b1;
+
+                if (address == 8'd255) 
+                    finished <= 1'b1; 
+            end
+            else
+                wren <= 1'b0; 
+        end
+    end
+
+
+endmodule
--- rtl/ksa.vhd
+++ rtl/ksa.vhd
@@ -17,21 +17,79 @@ entity ksa is
 end ksa;
 
 architecture rtl of ksa is
-   COMPONENT SevenSegmentDisplayDecoder IS
+
+    -- clock and reset signals  
+	signal clk, reset_n : std_logic;
+    signal address      : STD_LOGIC_VECTOR (7 downto 0);
+    signal data_in      : STD_LOGIC_VECTOR (7 downto 0);
+    signal wren         : std_logic; 
+    signal ram_initializer_finished : std_logic; 
+
+    signal secret_key   : STD_LOGIC_VECTOR (23 downto 0);
+
+
+
+	COMPONENT SevenSegmentDisplayDecoder IS
     PORT
     (
         ssOut : OUT STD_LOGIC_VECTOR (6 DOWNTO 0);
         nIn : IN STD_LOGIC_VECTOR (3 DOWNTO 0)
     );
     END COMPONENT;
-   
-    -- clock and reset signals  
-	 signal clk, reset_n : std_logic;										
+
+    COMPONENT s_memory IS
+	PORT
+	(
+		address		: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+		clock		: IN STD_LOGIC  := '1';
+		data		: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+		wren		: IN STD_LOGIC ;
+		q		: OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
+	);
+    END COMPONENT;
+
+	COMPONENT RamInitializer IS 
+	PORT 
+	(
+        clock       : IN STD_LOGIC;
+        reset       : IN STD_LOGIC; 
+        wren        : OUT STD_LOGIC;
+        address     : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+        data        : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+        finished    : OUT STD_LOGIC
+	);
+	END COMPONENT; 
+
 
 begin
-
     clk <= CLOCK_50;
     reset_n <= KEY(3);
+
+    secret_key (23 DOWNTO 10) <= (others => '0'); -- set upper bits to be 0 -- for task 2 
+    --secret_key[0] = 0x0
+    --secret_key[1] = 0x2
+    --secret_key[ 2] = 0x49
+	--00000000 00000010 01001001
+
+
+    s_memory_inst: s_memory   
+            port map (
+                address => address,
+                clock   => clk,
+                data    => data_in, 
+                wren    => wren,
+                q       => open 
+            );
+    
+    RamInitializer_inst: RamInitializer 
+            port map (
+                clock       => clk,
+                reset       => '0', --open 
+                wren        => wren,
+                address     => address,
+                data        => data_in, 
+                finished    => ram_initializer_finished
+            );
 
 end RTL;
 
